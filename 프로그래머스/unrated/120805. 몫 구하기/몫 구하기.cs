f74f6caadceec5be5aacc76deca9d13da1e24272
DDATMOUSE00 <149458680+DDATMOUSE00@users.noreply.github.com>
2023-11-23 09:45:36 +0900

    [level unrated] Title: 몫 구하기, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2, int sum = 0) {
+        if (num1 > 0 && num1 <= 100 && num2 > 0 && num2 <= 100)
+        {
+            sum = num1 / num2;
+        }
+        return sum;
+    }
+}
